c4958b887b0d39717eda2b7eee2374bda77bb0ca
WELL Sampaio <wsampaio@yahoo.com>
2018-12-10 11:44:36 -0200

    adicionada funcao para obter valores estatictica

--- cgi-bin/objetos/dbConn/CRUD.py
+++ cgi-bin/objetos/dbConn/CRUD.py
@@ -1,4 +1,4 @@
-#
+
 # Este arquivo é parte do programa multi_agenda
 #
 # Esta obra está licenciada com uma 
@@ -99,7 +99,6 @@ class CRUD():
 
 		return str
 
-
 	def delete(self, pk):
 		sql = \
 			"""
@@ -113,6 +112,9 @@ class CRUD():
 		self.__cursor.execute(sql, (pk, ))
 
 	def setSelect(self, pk, obj):
+		# Preenche os dados no objeto (obj) com os
+		# dados devolvidos pela consulta pela PK
+	
 		sql = \
 			"""
 			SELECT
@@ -131,7 +133,28 @@ class CRUD():
 		except ValueError:
 			pass
 
+	def getValue(self, sql, val):
+		# devolve o o primeiro valor do primeiro 
+		# registro da consulta enviada
+		# selecione um valor padrao (val) para retorno
+		# em caso de um valor None
+
+		value = val
+
+		try:
+			self.__cursor.executeList(sql)
+			value = self.__cursor.fetchone()
+		except ValueError:
+			pass
+
+		if value == None:
+			return val
+		else:
+			return value[0]
+
 	def getList(self, sql):
+		# Devolve uma array com objetos
+
 		lista = []
 
 		try:
@@ -142,9 +165,29 @@ class CRUD():
 			pass
 		return lista
 
+	def getIndex(self, sql):
+		# Devolve uma array soh com as PKs
+
+		listaIndex = []
+
+		try:
+			self.__cursor.executeList(sql)
+			for array in self.__cursor.fetchall():
+				listaIndex.append(array[0])
+		except ValueError:
+			pass
+		return listaIndex
 
 	def setStatement(self, obj, sql):
+		# vai puxar os dados do objeto para executar
+		# um comando SQL para o BD
 
+		# context = valores que serao passados ao sqlite
+		# junto com a string SQL
+		context = []
+		registroNovo = False
+	
+		# define os metodos GETTER e SETTER da PK
 		getPk = getattr(obj,
 				"get" +
 				self.__pk[:1].upper() +
@@ -157,17 +200,21 @@ class CRUD():
 				self.__pk[1:]
 		)
 
-		#verifica se a PK é igual a zero
-		#e puxa a autonumeração
+		
+		# verifica se a PK é igual a zero
+		# e puxa a autonumeração
 		if int(getPk()) < 1:
 			setPk(self.__conn.autoNumeracao())
-		else:
-			context.append(getPk())
+			registroNovo = True
+		
 
-		context = []
+		
 
+		# ratreia os métodos do objeto com a ajuda
+		# da lista com nomes dos campos da tabela
+		# e adiciona os dados do objeto a array context
+		
 		i = 0
-
 		for nomeCampo in self.nomeCampos:
 
 			getMethod = getattr(obj,
@@ -175,27 +222,45 @@ class CRUD():
 					nomeCampo[:1].upper() +
 					nomeCampo[1:]
 			)
-
-			#print("")
-			#print(self.tipoCampos[i])
-			#print(nomeCampo)
-
+			
+			
+			# Verifica o tipo de campo do modo que esta
+			# registrado no SQLite e formata o registro
+			
 			if self.tipoCampos[i] == "LocalDateTime":
 				context.append(
-					#FormatData.para_JDate(getMethod())
-					getMethod()
+					FormatData.para_JDate(getMethod())
+					#getMethod()
 				)
 			elif self.tipoCampos[i] == "BOOLEAN":
-				context.append(
-					str(getMethod())
-				)
+				if getMethod():
+					context.append(1)
+				else:
+					context.append(0)
 			else:
 				context.append(getMethod())
 
 			i += 1
+			
+			
+			# TESTES
+			#print("")
+			#print(self.tipoCampos[i])
+			#print(nomeCampo)
 
+		
+		# precisa incluir mais uma vez o valor da PK
+		# em caso de um UPDATE pois a array de campos do BD
+		# inclui a condicao WHERE no final da String
+		if registroNovo == False:
+			context.append(getPk())
+
+			
+			
+			
+		#TESTES
 		#print(sql)
-		print(context)
+		#print(context)
 
 		self.__cursor.execute(sql, context)
 
