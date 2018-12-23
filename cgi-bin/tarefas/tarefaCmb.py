0bdd1de6b72e65f03b892fd347b4889c876df3c6
WELL Sampaio <wsampaio@yahoo.com>
2018-12-23 12:00:10 +0000

    add JSON tarefaCmb

---
+++ cgi-bin/tarefas/tarefaCmb.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+#
+# Este arquivo é parte do programa multi_agenda
+#
+# Esta obra está licenciada com uma 
+# Licença Creative Commons Atribuição 4.0 Internacional.
+# (CC BY 4.0 Internacional)
+#  
+# Para ver uma cópia da licença, visite
+# https://creativecommons.org/licenses/by/4.0/legalcode
+# 
+# WELLINGTON SAMPAIO - wsampaio@yahoo.com
+# https://www.linkedin.com/in/wellsampaio/
+#
+
+import sys
+from os.path import dirname, realpath, sep, pardir
+
+sys.path.append((dirname(realpath(__file__)) + sep + pardir))
+
+import cgitb
+cgitb.enable()
+
+from objetos.tarefas.Tarefa import Tarefa
+from objetos.tarefas.TarefaDAO import TarefaDAO
+
+tarefa = Tarefa()
+tarefaDAO = TarefaDAO()
+
+print("Content-type: application/json\n")
+
+saida = ""
+
+saida += """
+{
+	"tarefas": ["""
+
+i = 0
+#lista = tarefaDAO.listaPrincipaisEmAberto()
+lista = tarefaDAO.getListaCmb()
+
+contaLista = len(lista) -1
+
+for obj in lista:
+
+	saida += """
+		{}
+			"codTarefa": {},
+                        "tarefa": "{}"
+		{}""".format(
+			"{",
+			obj.getCodTarefa(),
+			obj.getTarefa()
+				.replace("\r", "%r")
+				.replace("\n", "%n")
+				.replace("\t", "$t")
+				.replace("\\", "$b")
+				.replace("\"", "\\\""),
+			"}"
+		)
+
+
+	if i < contaLista:
+		saida += ","
+		i += 1
+	else:
+		break
+
+saida += """
+	]
+}
+"""
+
+
+print(
+	saida
+		.replace("\n", "")
+		.replace("\t", "")
+)
