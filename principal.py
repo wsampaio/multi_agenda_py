ec58859d07d9e07c3eb115ff8dbeae8a7851af4c
wsampaio <well7cordas@yahoo.com.br>
2019-08-08 05:35:23 -0300

    add Termos de Licenca e Dados do Servidor

--- principal.py
+++ principal.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+#  
+# Este arquivo é parte do programa multi_agenda
+#  
+# Esta obra está licenciada com uma
+# Licença Creative Commons Atribuição 4.0 Internacional.
+# (CC BY 4.0 Internacional)
+#  
+# Para ver uma cópia da licença, visite
+# https://creativecommons.org/licenses/by/4.0/legalcode
+#  
+# WELLINGTON SAMPAIO - wsampaio@yahoo.com
+# https://www.linkedin.com/in/wellsampaio/
+#  
 
 import http.server
 import socketserver
@@ -14,31 +24,13 @@ import cgi
 
 import sys
 import os
+import socket
 
 current_path = os.getcwd()
-#current_path = current_path.replace('/', '.')
 current_path += ('/objetos')
 current_path = os.path.join(sys.path[0], 'objetos')
-#current_path = sys.path[0]
-#current_path = "/home/wsampaio/Documentos/NetBeansProjects/multi_agenda_py"
 
-print(current_path)
-
-
-
-
-#print('Python %s on %s' % (sys.version, sys.platform))
-#sys.path.extend([WORKING_DIR_AND_PYTHON_PATHS])
-#sys.path.extend(current_path)
-#sys.path.append(current_path)
-#sys.path.append('objetos/')
-
-#sys.path.append('/usr/bin/python2.7/dist-packages')
-
-print(sys.path)
-#print(sys.modules.keys())
-#print(globals().items())
-
+#print(current_path)
 
 def abreServidor ():
 
@@ -49,7 +41,14 @@ def abreServidor ():
 	handler.cgi_directories = ['/cgi-bin']
 
 	try:
-		with HTTPServer(server_address,CGIHTTPRequestHandler) as httpd:
+		print("\nHostname:") 
+		print(socket.gethostname() + "\n")
+
+		print("IP Address:")
+		os.system("hostname -I")
+		print("\n")
+
+		with HTTPServer(server_address, CGIHTTPRequestHandler) as httpd:
 			#httpd.shutdown()
 			print('Python %s on %s' % (sys.version, sys.platform))
 			print("serving at port", PORT)
@@ -59,28 +58,9 @@ def abreServidor ():
 		httpd.shutdown()
 
 
-def abreServidor3 ():
-	print("VAI CORINTHIANS!")
-	tarefa = Tarefa.Tarefa()
-	tarefaDAO = TarefaDAO.TarefaDAO()
-
-	#tarefa = tarefaDAO.select(381)
-
-
-	print(tarefa.getCodTarefa())
-	print(tarefa.getTarefa())
-
-
-	tarefa.setTarefa("MUDEI!!!")
-	tarefa.setCodTarefa(0)
-	#print(tarefaDAO.insert(tarefa))
-
-	print(tarefa.getTarefa())
-	print(tarefa.getTerminado())
-
-
 if __name__ == "__main__":
-	print("Hello World")
+	os.system("clear")
+	print("\nmulti_agenda_py.server")
 	abreServidor()
 
 
